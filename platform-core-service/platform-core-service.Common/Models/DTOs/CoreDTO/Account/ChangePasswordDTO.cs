20ee8f4c1102368eeef641dc3f8f1a2f686f3d26
ttoann <ntt26.5.2004@gmail.com>
2026-03-11 02:28:00 +0700

    feat: implement user change password API

---
+++ platform-core-service/platform-core-service.Common/Models/DTOs/CoreDTO/Account/ChangePasswordDTO.cs
@@ -0,0 +1,29 @@
+﻿using platform_core_service.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace platform_core_service.Common.Models.DTOs.CoreDTO
+{
+    public class ChangePasswordDTO
+    {
+        [TrimmedRequired]
+        [MinLength(12)]
+        [MaxLength(128)]
+        public string OldPassword { get; set; }
+
+        [TrimmedRequired]
+        [MinLength(12)]
+        [MaxLength(128)]
+        public string NewPassword { get; set; }
+
+        [TrimmedRequired]
+        [MinLength(12)]
+        [MaxLength(128)]
+        [Compare(nameof(NewPassword), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
